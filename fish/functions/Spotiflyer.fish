96e279ed8d0f4ea8af74da6c9c0641b057d270d1
LegendRawnak <120038581+LegendRawnak@users.noreply.github.com>
2023-02-10 11:33:53 +0600

    Delete fish directory

--- fish/functions/Spotiflyer.fish
+++
@@ -1,4 +0,0 @@
-function Spotiflyer --wraps='java -jar ~/Downloads/SpotiFlyer-linux-x64-3.6.3.jar' --description 'alias Spotiflyer=java -jar ~/Downloads/SpotiFlyer-linux-x64-3.6.3.jar'
-  java -jar ~/Downloads/SpotiFlyer-linux-x64-3.6.3.jar $argv
-        
-end
